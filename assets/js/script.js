7961932506876a1e0bc83e29f59f913371f62139
VIMATH KUMAR <123919808+vimathkumar@users.noreply.github.com>
2023-03-10 13:44:25 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Python Developer", "Java Developer", "Django Developer"],
+    strings: ["Python Developer", "Web Developer", "Django Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
